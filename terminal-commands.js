75dd6c94f9717cc6ee63d3cccf4720ab386b9a20
David Gentile <davidgentile@Davids-MBP.attlocal.net>
2019-01-28 20:21:57 -0500

    Set up file

--- terminal-commands.js
+++ terminal-commands.js
@@ -2,10 +2,18 @@ const fs = require('fs');
 
 module.exports.ls = () => {
     fs.readdir('./', (err, files) => {
-        const filestToString = files.reduce((acc,file) => {
+        const filesToString = files.reduce((acc,file) => {
             return `${acc} ${file} `;
         }, '');
 
-        console.log(filestToString);
+        console.log(filesToString);
     });
+};
+
+module.exports.touch = () => {
+    // your code here
+};
+
+module.exports.mkdir = () => {
+    // your code here
 };
